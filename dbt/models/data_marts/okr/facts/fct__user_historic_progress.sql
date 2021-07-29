5e0d7e090325418b51b1f5c13df465c58b33c8b1
delucca <delucca@protonmail.com>
2021-07-29 19:39:15 -0300

    feat(dbt): finishes progress for user

--- dbt/models/data_marts/okr/facts/fct__user_historic_progress.sql
+++ dbt/models/data_marts/okr/facts/fct__user_historic_progress.sql
@@ -3,10 +3,26 @@ with
     select * from {{ ref('dim__user') }}
   ),
 
+  key_result as (
+    select * from {{ ref('dim__key_result') }}
+  ),
+
+  key_result_latest_check_in as (
+    select * from {{ ref('fct__key_result_latest_check_in') }}
+  ),
+
+  key_result_check_in as (
+    select * from {{ ref('dim__key_result_check_in') }}
+  ),
+
   final as (
     select
-      users.id as user_id
+      users.id as user_id,
+      {{ calculate_progress('key_result_check_in', 'key_result') }} as progress
       from users
+      left join key_result on users.id = key_result.owner_id
+      left join key_result_latest_check_in on key_result.id = key_result_latest_check_in.key_result_id
+      left join key_result_check_in on key_result_latest_check_in.key_result_check_in_id = key_result_check_in.id
   )
 
 select * from final
