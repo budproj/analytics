c9b1b6af1232c88be0bb8a41ff0c582fd97afe50
delucca <delucca@protonmail.com>
2021-07-29 17:59:34 -0300

    feat(dbt): adds company to cycle

--- dbt/models/data_marts/okr/dimensions/dim__cycle.sql
+++ dbt/models/data_marts/okr/dimensions/dim__cycle.sql
@@ -3,8 +3,21 @@ with
     select * from {{ ref('stg_okr__cycle') }}
   ),
 
+  team as (
+    select * from {{ ref('dim__team') }}
+  ),
+
+  company as (
+    select * from {{ ref('dim__company') }}
+  ),
+
   final as  (
-    select * from stg_okr__cycle
+    select
+      stg_okr__cycle.*,
+      company.id as company_id
+      from stg_okr__cycle
+      left join team on stg_okr__cycle.team_id = team.id
+      left join company on team.company_id = company.id
   )
 
 select * from final
