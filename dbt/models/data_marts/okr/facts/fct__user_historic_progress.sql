c2812b6e7d76e553fbdd8261d149cd1153490827
delucca <delucca@protonmail.com>
2021-07-29 20:00:59 -0300

    feat(dbt): adds date to progress

--- dbt/models/data_marts/okr/facts/fct__user_historic_progress.sql
+++ dbt/models/data_marts/okr/facts/fct__user_historic_progress.sql
@@ -18,7 +18,8 @@ with
   final as (
     select
       users.id as user_id,
-      {{ calculate_progress('key_result_check_in', 'key_result') }} as progress
+      {{ calculate_progress('key_result_check_in', 'key_result') }} as progress,
+      key_result_check_in.created_at as date
       from users
       left join key_result on users.id = key_result.owner_id
       left join key_result_latest_check_in on key_result.id = key_result_latest_check_in.key_result_id
