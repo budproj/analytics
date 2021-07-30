e9c339a1c9a61a8c4cbe0a2c5734c414b352d8bf
delucca <delucca@protonmail.com>
2021-07-30 12:56:17 -0300

    feat(dbt): fixes duplicated users

--- dbt/models/data_marts/okr/facts/fct__user_cycle_progress.sql
+++ dbt/models/data_marts/okr/facts/fct__user_cycle_progress.sql
@@ -7,6 +7,10 @@ with
     select * from {{ ref('dim__key_result') }}
   ),
 
+  cycle as (
+    select * from {{ ref('dim__cycle') }}
+  ),
+
   key_result_latest_check_in as (
     select * from {{ ref('fct__key_result_latest_check_in') }}
   ),
@@ -15,24 +19,18 @@ with
     select * from {{ ref('dim__key_result_check_in') }}
   ),
 
-  user_progress_in_key_result as (
+  final as (
     select
       users.id as user_id,
+      cycle.id as cycle_id,
       {{ calculate_progress('key_result_check_in', 'key_result') }} as progress,
       key_result_check_in.created_at as date
       from users
       left join key_result on users.id = key_result.owner_id
       left join key_result_latest_check_in on key_result.id = key_result_latest_check_in.key_result_id
       left join key_result_check_in on key_result_latest_check_in.key_result_check_in_id = key_result_check_in.id
-  ),
-
-  final as (
-    select
-      user_id,
-      avg(progress) as progress,
-      max(date) as date
-      from user_progress_in_key_result
-      group by user_id
+      left join cycle on key_result.cycle_id = cycle.id
+      where cycle.active = true
   )
 
 select * from final
