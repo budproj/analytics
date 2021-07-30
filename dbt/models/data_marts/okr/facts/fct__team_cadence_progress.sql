f22e7c48e63a0459e2b70dba5d42ec5d40e802d3
delucca <delucca@protonmail.com>
2021-07-30 13:17:09 -0300

    feat(dbt): adds team cadence progress and fixes some issues

--- dbt/models/data_marts/okr/facts/fct__team_cadence_progress.sql
+++ dbt/models/data_marts/okr/facts/fct__team_cadence_progress.sql
@@ -1,6 +1,6 @@
 with
-  users as (
-    select * from {{ ref('dim__user') }}
+  team as (
+    select * from {{ ref('dim__team') }}
   ),
 
   key_result as (
@@ -19,48 +19,48 @@ with
     select * from {{ ref('dim__key_result_check_in') }}
   ),
 
-  user_key_result_progress as (
+  team_key_result_progress as (
     select
-      users.id as user_id,
+      team.id as team_id,
       cycle.id as cycle_id,
       cycle.date_start as cycle_date_start,
       cycle.cadence as cadence,
       {{ calculate_progress('key_result_check_in', 'key_result') }} as progress,
       key_result_check_in.created_at as date
-      from users
-      left join key_result on users.id = key_result.owner_id
+      from team
+      left join key_result on team.id = key_result.team_id
       left join key_result_latest_check_in on key_result.id = key_result_latest_check_in.key_result_id
       left join key_result_check_in on key_result_latest_check_in.key_result_check_in_id = key_result_check_in.id
       left join cycle on key_result.cycle_id = cycle.id
   ),
 
-  user_progress_in_cycle as (
+  team_progress_in_cycle as (
     select
-      user_id,
+      team_id,
       cycle_id,
       cycle_date_start,
       cadence,
       avg(progress) as progress,
       max(date) as date
-      from user_key_result_progress
-      group by user_id, cycle_id, cycle_date_start, cadence
+      from team_key_result_progress
+      group by team_id, cycle_id, cycle_date_start, cadence
   ),
 
-  user_ranked_cadence as (
+  team_ranked_cadence as (
     select
       *,
-      row_number() over (partition by user_id, cadence order by cycle_date_start desc) as rank
-      from user_progress_in_cycle
+      row_number() over (partition by team_id, cadence order by cycle_date_start desc) as rank
+      from team_progress_in_cycle
   ),
 
   final as (
     select
-      user_id,
+      team_id,
       cycle_id,
       cadence,
       progress,
       date
-      from user_ranked_cadence
+      from team_ranked_cadence
       where rank = 1
   )
 
