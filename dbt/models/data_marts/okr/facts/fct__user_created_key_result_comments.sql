da721dead5b3b5c7c9c273d5dcc038bac802a4f9
delucca <delucca@protonmail.com>
2021-08-03 11:45:25 -0300

    bugfix(dbt): fixes kr comments nullable values

--- dbt/models/data_marts/okr/facts/fct__user_created_key_result_comments.sql
+++ dbt/models/data_marts/okr/facts/fct__user_created_key_result_comments.sql
@@ -12,8 +12,8 @@ with
       users.id as user_id,
       key_result_comment.id as key_result_comment_id,
       key_result_comment.created_at as created_at
-      from users
-      left join key_result_comment on users.id = key_result_comment.user_id
+      from key_result_comment
+      left join users on users.id = key_result_comment.user_id
   )
 
 select * from final
