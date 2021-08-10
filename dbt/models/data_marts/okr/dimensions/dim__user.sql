237692d12265c57404bd56c7761c4178bfbfabd2
delucca <delucca@protonmail.com>
2021-08-10 19:02:09 -0300

    feat(dbt): improves facts by decoupling access from user

--- dbt/models/data_marts/okr/dimensions/dim__user.sql
+++ dbt/models/data_marts/okr/dimensions/dim__user.sql
@@ -19,30 +19,9 @@ with
     select * from stg_okr__user
   ),
 
-  first_pageview_by_user as (
-    select 
-      user_id,
-      min(event_time) as event_time
-      from amplitude_event
-      where event_type = 'PageView'
-      group by user_id
-  ),
-
-  latest_pageview_by_user as (
-    select 
-      user_id,
-      max(event_time) as event_time
-      from amplitude_event
-      where event_type = 'PageView'
-      group by user_id
-  ),
-
   final as (
     select
       users.*,
-      fe.event_time as first_access_time,
-      le.event_time as last_access_time,
-      date_part('day', fe.event_time::timestamp - created_at::timestamp)::float as activation_delta_days,
       case
         when (
           select count(*) from seed_buddy_users_email where email = users.email
@@ -52,9 +31,7 @@ with
         ) = 1 then 'SANDBOX'
         else 'CUSTOMER'
       end as type
-      from users
-        left join first_pageview_by_user fe on fe.user_id = users.id
-        left join latest_pageview_by_user le on le.user_id = users.id
+    from users
   )
 
 select * from final
--- dbt/models/data_marts/okr/facts/fct__company_active_progress.sql
+++ dbt/models/data_marts/okr/facts/fct__company_active_progress.sql
@@ -25,7 +25,7 @@ with
       {{ calculate_progress('key_result_check_in', 'key_result') }} as progress,
       key_result_check_in.created_at as date
       from company
-      left join key_result on company.id = key_result.team_id
+      left join key_result on company.team_id = key_result.team_id
       left join key_result_latest_check_in on key_result.id = key_result_latest_check_in.key_result_id
       left join key_result_check_in on key_result_latest_check_in.key_result_check_in_id = key_result_check_in.id
       left join cycle on key_result.cycle_id = cycle.id
