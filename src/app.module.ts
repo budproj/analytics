fff82b9eef60012f03a61534db60f6c1709b4e62
delucca <delucca@protonmail.com>
2021-08-13 14:42:51 -0300

    chore: adding db structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 
-import { GRPCConfigModule } from './infrastructure/config/grpc/grpc.module'
-import { LoggingConfigModule } from './infrastructure/config/logging/logging.module'
+import { GRPCConfigModule } from './config/grpc/grpc.module'
+import { LoggingConfigModule } from './config/logging/logging.module'
 import { GRPCModule } from './infrastructure/grpc/grpc.module'
 
 @Module({
