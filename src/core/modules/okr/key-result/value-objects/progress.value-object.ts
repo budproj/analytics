6d8e12fb723d9e9636081e7bdf446537ed0cd72f
delucca <delucca@protonmail.com>
2021-08-25 17:39:39 +0000

    bugfix(progress): fixes minmax value

--- src/core/modules/okr/key-result/value-objects/progress.value-object.ts
+++ src/core/modules/okr/key-result/value-objects/progress.value-object.ts
@@ -1,9 +1,21 @@
+import { max, min } from 'lodash'
+
 import { DomainPrimitive } from '@core/common/domain/interfaces/domain-primitive.interface'
 import { ValueObject } from '@core/common/domain/value-objects/base.value-object'
 import { ArgumentInvalidException } from '@core/common/exceptions/argument-invalid.exception'
 
 export class Progress extends ValueObject<number> {
-  constructor(value: number) {
+  static get min(): number {
+    return 0
+  }
+
+  static get max(): number {
+    return 1
+  }
+
+  constructor(primitiveValue: number) {
+    const value = min([max([primitiveValue, Progress.min]), Progress.max])
+
     super({ value })
   }
 
@@ -16,7 +28,7 @@ export class Progress extends ValueObject<number> {
   }
 
   protected validate({ value }: DomainPrimitive<number>): void {
-    if (Number.isNaN(value) || value < 0 || value > 1) {
+    if (Number.isNaN(value)) {
       throw new ArgumentInvalidException('Incorrect progress format')
     }
   }
