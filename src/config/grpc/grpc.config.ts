42a655cc3903306c2bf67ad7ced482f7e710c742
delucca <delucca@protonmail.com>
2021-08-30 21:17:27 -0300

    testing credentials

--- src/config/grpc/grpc.config.ts
+++ src/config/grpc/grpc.config.ts
@@ -1,3 +1,4 @@
+import { ServerCredentials } from '@grpc/grpc-js'
 import { registerAs } from '@nestjs/config'
 
 import { GRPCConfig } from './grpc.interface'
@@ -6,6 +7,7 @@ export const grpcConfig = registerAs(
   'grpc',
   (): GRPCConfig => ({
     port: Number.parseInt(process.env.GRPC_PORT, 10),
+    credentials: ServerCredentials.createInsecure(),
     url: `localhost:${Number.parseInt(process.env.GRPC_PORT, 10)}`,
     package: process.env.GRPC_PACKAGES.split(','),
     protoPath: process.env.GRPC_PROTO_PATHS.split(','),
--- src/config/grpc/grpc.interface.ts
+++ src/config/grpc/grpc.interface.ts
@@ -1,5 +1,8 @@
+import { ServerCredentials } from '@grpc/grpc-js'
+
 export interface GRPCConfig {
   port: number
+  credentials: ServerCredentials
   url: string
   package: string[]
   protoPath: string[]
